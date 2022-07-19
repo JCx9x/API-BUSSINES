5d1f901fcfbe77c9c4c06f389b4ba341faf9993d
JCx9x <javicanoramirez@gmail.com>
2022-07-19 16:28:23 +0200

    Se ha cargado el proyecto

---
+++ spring-boot-backend-apirest/src/main/java/com/example/demo/controllers/ClienteRestController.java
@@ -0,0 +1,134 @@
+package com.example.demo.controllers;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.entity.Cliente;
+import com.example.demo.models.services.IClienteService;
+
+@CrossOrigin(origins = { "http://localhost:8081" })
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/clientes")
+	public List<Cliente> index() {
+		return clienteService.findAll();
+	}
+	
+	@GetMapping("/clientes/{id}")
+	//@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		
+		Cliente cliente = null;
+		Map <String, Object> response = new HashMap<>();
+		
+		//Manejo de errores en bbdd
+		try {
+			cliente = clienteService.findById(id);	
+		} catch(DataAccessException e) {
+			response.put("mensaje","Error al realizar consulta en BBDD");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		
+		//Manejo de causisticas que generarian error
+		if(cliente == null) {
+			response.put("mensaje","El cliente ID; ".concat(id.toString().concat(" no existe en la BBDD")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?>  create(@RequestBody Cliente cliente) {
+		
+		Cliente clienteNew = null;
+		Map <String, Object> response = new HashMap<>();
+		
+		try {
+			clienteNew = clienteService.save(cliente);
+		}catch(DataAccessException e) {
+			response.put("mensaje","Error al realizar el insert en la BBDD");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("", "El cliente ha sido creado con éxito");
+		response.put("cliente", clienteNew);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("clientes/{id}")
+	public ResponseEntity<?>  update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		
+		Cliente clienteActual = clienteService.findById(id);
+		Cliente clienteUpdated = null;
+		Map <String, Object> response = new HashMap<>();
+		
+		//Manejo de causisticas que generarian error
+		if(clienteActual == null) {
+			response.put("mensaje","Error: no se puede editar, l cliente ID; ".concat(id.toString().concat(" no existe en la BBDD")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		try {
+			
+			clienteActual.setApellido(cliente.getApellido());
+			clienteActual.setNombre(cliente.getNombre());
+			clienteActual.setEmail(cliente.getEmail());
+			
+			clienteUpdated = clienteService.save(clienteActual);
+			
+		}catch(DataAccessException e) {
+			response.put("mensaje","Error al actualizar en la BBDD");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	
+		response.put("", "El cliente ha sido actualizado con éxito");
+		response.put("cliente", clienteUpdated);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("clientes/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		
+		Map <String, Object> response = new HashMap<>();
+		
+		try {
+			clienteService.delete(id);	
+		}catch(DataAccessException e) {
+			response.put("mensaje","Error al eliminar en la BBDD");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}
+	
+}
